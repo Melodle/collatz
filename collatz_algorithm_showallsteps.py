abf13638dbb35b8484830ff65cfd930e89e5128a
Bolzmann\Louis <Louisp.huber@gmx.ch>
2017-07-03 13:14:31 +0200

    little improvement

--- collatz_algorithm_showallsteps.py
+++ collatz_algorithm_showallsteps.py
@@ -1,7 +1,8 @@
 #This program shows the steps of the collatz-algorithm with the number z
 def collatz(z):
+    print("The number to calculate is",z)
     i=0
-    while z!=1:    
+    while z!=1:
         #the collatz algorithm is executed and the number of steps is indicated by "i"
         if z%2 ==0:
             z=z//2
@@ -15,6 +16,5 @@ def collatz(z):
             print("step",i,"uneven number")
             print(z)
     return i
-
 A=collatz(17)
 print("The number of steps the algorithm needs with number 17 is", A)
